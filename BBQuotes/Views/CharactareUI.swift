cdf6a68fcecb0f5dc1dd4b784ab0ccd3dc9d4a85
Chris <san.kyu@icloud.com>
2024-09-23 14:31:26 -0600

    Character view added

---
+++ BBQuotes/Views/CharactareUI.swift
@@ -0,0 +1,18 @@
+//
+//  CharactareUI.swift
+//  BBQuotes
+//
+//  Created by chris on 2024/09/23.
+//
+
+import SwiftUI
+
+struct CharactareUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharactareUI()
+}
